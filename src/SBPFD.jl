a656317560be554684ee7f7bb75494bee98d3753
Jeremy E Kozdon <jekozdon@nps.edu>
2020-08-10 13:02:19 -0700

    WIP

--- src/SBPFD.jl
+++ src/SBPFD.jl
@@ -27,6 +27,18 @@ struct FirstDerivativeDiagonalSBP{PI, PB, T, B, C, BC, D}
   end
 end
 
+#=
+@ARTICLE{Strand94,
+  author = {B. Strand},
+  title = {Summation by parts for finite difference approximations for $d/dx$},
+  journal = {Journal of Computational Physics},
+  year = {1994},
+  volume = {110},
+  pages = {47--67},
+  number = {1},
+  doi = {10.1006/jcph.1994.1005}
+}
+=#
 FirstDerivativeDiagonalSBP{T, 2}() where {T} =
   FirstDerivativeDiagonalSBP{T, 2, 1}()
 function FirstDerivativeDiagonalSBP{
@@ -38,6 +50,37 @@ function FirstDerivativeDiagonalSBP{
   DB = SMatrix{1, 2, T}([-1 1])
   DI = SVector{3, T}([-1 // 2, 0, 1 // 2])
   FirstDerivativeDiagonalSBP{2, 1}(HB, DB, DI)
+end
+
+#=
+@ARTICLE{Strand94,
+  author = {B. Strand},
+  title = {Summation by parts for finite difference approximations for $d/dx$},
+  journal = {Journal of Computational Physics},
+  year = {1994},
+  volume = {110},
+  pages = {47--67},
+  number = {1},
+  doi = {10.1006/jcph.1994.1005}
+}
+=#
+FirstDerivativeDiagonalSBP{T, 4}() where {T} =
+  FirstDerivativeDiagonalSBP{T, 4, 2}()
+function FirstDerivativeDiagonalSBP{
+  T,
+  4,
+  2,
+}() where {T <: Union{AbstractFloat, Rational}}
+  #! format: off
+  HB = SVector{4, T}([17 // 48, 59 // 48, 43 // 48, 49 // 48])
+  DB = SMatrix{4, 6, T}([ -24 // 17  59 // 34  -4 // 17 -3 // 34  0        0
+                           -1 // 2    0         1 // 2   0        0        0
+                            4 // 43 -59 // 86   0       59 // 86 -4 // 43  0
+                            3 // 98   0       -59 // 98  0       32 // 49 -4 // 49
+                           ])
+  DI = SVector{5, T}([1 // 12 -2 // 3 0 2 // 3 -1 // 12])
+  #! format: on
+  FirstDerivativeDiagonalSBP{4, 2}(HB, DB, DI)
 end
 
 end # module
